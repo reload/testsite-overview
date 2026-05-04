55a01c7926e21ad0053ed2f842693b28b74b1e00
Arne Jørgensen <arne@arnested.dk>
2026-05-04 11:55:37 +0200

    Remember to generate templ code

--- page_templ.go
+++ page_templ.go
@@ -63,9 +63,9 @@ func Env(env Environment) templ.Component {
 						return templ_7745c5c3_Err
 					}
 					var templ_7745c5c3_Var3 templ.SafeURL
-					templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinURLErrs("https://" + route.Href)
+					templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinURLErrs(route.Href)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `page.templ`, Line: 15, Col: 43}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `page.templ`, Line: 15, Col: 30}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 					if templ_7745c5c3_Err != nil {
@@ -78,7 +78,7 @@ func Env(env Environment) templ.Component {
 					var templ_7745c5c3_Var4 string
 					templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(route.Href)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `page.templ`, Line: 15, Col: 74}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `page.templ`, Line: 15, Col: 61}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 					if templ_7745c5c3_Err != nil {
